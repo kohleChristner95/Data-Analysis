c340d9f0d4bdf59ef622065aa368ae6810c2de8a
Kohle Christner <80296696+kohleChristner95@users.noreply.github.com>
2022-05-29 22:07:04 -0400

    Started adding montly goal section

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,13 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 function render() {
-  ReactDOM.render(<h2>Hello from React!</h2>, document.body);
+  ReactDOM.render(
+    <>
+      <h1>Monthly Goal</h1>
+      <div id="graph">look here</div>
+    </>,
+    document.body
+  );
 }
 
 render();
