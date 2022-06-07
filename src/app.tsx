3f826e92a34180f2cbd9e16f3c7103e12abb18ac
Kohle Christner <80296696+kohleChristner95@users.noreply.github.com>
2022-06-06 21:38:19 -0400

    Making more progress on monthly-goal UI.

--- src/app.tsx
+++ src/app.tsx
@@ -3,10 +3,10 @@ import * as ReactDOM from "react-dom";
 
 function render() {
   ReactDOM.render(
-    <>
+    <div id="monthly-goal" className="section">
       <h1>Monthly Goal</h1>
-      <div id="graph">look here</div>
-    </>,
+      <div id="progress-bar" className="center"></div>
+    </div>,
     document.body
   );
 }
