4fff0401585d9f770f9cdbe5634e24e75ae8aa04
Kohle Christner <80296696+kohleChristner95@users.noreply.github.com>
2022-06-14 22:13:54 -0400

    Making more progress. Created MonthlyGoalsSections component

---
+++ src/Components/MonthlyGoalSections.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+type monthlyProps = {
+  title: string;
+  currency: number;
+  percentage: number;
+};
+
+const MonthlyGoalSections = ({ title, currency, percentage }: monthlyProps) => {
+  return (
+    <div className="monthly-goal-section">
+      <div>{title}</div>
+      <div>% {percentage}</div>
+      <div>💲{currency}</div>
+    </div>
+  );
+};
+
+export default MonthlyGoalSections;
--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,28 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import MonthlyGoalSections from "./Components/MonthlyGoalSections";
 
 function render() {
   ReactDOM.render(
     <div id="monthly-goal" className="section">
       <h1>Monthly Goal</h1>
       <div id="progress-bar" className="center"></div>
+      <MonthlyGoalSections title={"Goal"} currency={5500} percentage={80} />
+      <MonthlyGoalSections
+        title={"Gifts Started"}
+        currency={4518}
+        percentage={82}
+      />
+      <MonthlyGoalSections
+        title={"Commitments"}
+        currency={4518}
+        percentage={82}
+      />
+      <MonthlyGoalSections
+        title={"Below Goal"}
+        currency={982}
+        percentage={18}
+      />
     </div>,
     document.body
   );
